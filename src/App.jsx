feb0823e9e1bada985563ca98653966515856874
Rene A Youssef <Reneabdo10@gmail.com>
2025-05-08 10:43:58 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,116 @@
-import "./App.css";
-import Header from "./Components/Header.jsx";
-import Entry from "./Components/Entry.jsx";
-import Contact from "./Components/Contact.jsx";
-import Joke from "./Components/joke.jsx";
+import jokesData from "./jokesData.js";
+import Joke from "./components/joke.jsx";
 export default function App() {
+  const jokeElements = jokesData.map((joke) => {
+    //we will take the array of regular javascript object and turn them into an array of joke elements
+    return <Joke setup={joke.setup} punchline={joke.punchline} />;
+  });
   return (
-    // <div className="contact">
-    //   <Contact
-    //    img="src/assets/me.jpg"
-    //    name="Rene Youssef"
-    //    phone="+961 89 897 974"
-    //    email="reneYoussef10@gmail.com"
-    //    />
-    //   <Contact
-    //   img="src/assets/me.jpg"
-    //   name="Elie Youssef"
-    //   phone="+961 79 118 999"
-    //   email="elie10@gmail.com"
-    //   />
-    //   <Contact
-    //   img="src/assets/me.jpg"
-    //   name="Charbel Youssef"
-    //   phone="+961 81 324 974"
-    //   email="CharbelYoussef10@gmail.com"
-    //   />
-    //   <Contact
-    //   img="src/assets/me.jpg"
-    //   name="George Youssef"
-    //   phone="+961 71 332 231"
-    //   email="GeorgeYoussef10@gmail.com"
-    //   />
-    // </div>
-
     <>
-      <main>
-        <Joke 
-        setup ="I told my wife she was drawing her eyebrows too high—she looked surprised." 
-        punchline="Surprised? More like shocked!"
-        upVotes={10}
-        isPun={true}
-      comment={[
-        {author:"Rene", text:"I love this joke!"},
-        {author:"Elie", text:"I love this joke!"}
-      ]}
-         />
-        <Joke 
-        setup ="Parallel lines have so much in common—it's a shame they'll never meet." 
-        punchline="It’s a tragic tale of unrequited geometry."
-        isPun={true}
-         />
-        <Joke 
-        setup ="I’m on a seafood diet—I see food and I eat it." 
-        punchline="I’m doing it for the 'crab' in me."
-        isPun={true}
-         />
-        <Joke
-         setup ="I used to play piano by ear, but now I use my hands." 
-         punchline="Guess that’s what happens when you go off the scales."
-         isPun={false}
-          />
-        <Joke 
-        setup ="I’m reading a book on anti-gravity—it’s impossible to put down." 
-        punchline="It’s a real uplifting experience!"
-        isPun={true}
-         />
-         <Joke 
-        punchline="It’s a real uplifting experience!"
-        isPun={false}
-         />
-      </main>
+      <main>{jokeElements}</main>
     </>
   );
 }
+
+// const persons = [
+//   <h2>Rene</h2>,
+//   <h2>Elie</h2>,
+//   <h2>Charbel</h2>,
+//   <h2>George</h2>,
+// ];
+
+/* 
+      <Header />
+      <main className="container">
+
+        <Entry
+        img = {{src: "src/assets/me.jpg", alt: "Mount Fuji"}}
+        title="Mount Fuji"
+        country="Japan"
+        googleMapsUrl="https://www.google.com/maps/place/Japan"
+        dates="12 Jan, 2021 - 24 Jan, 2021"
+        description="Mount Fuji is Japan's highest peak, standing at 3,776 meters (12,389 feet). It is an active stratovolcano that last erupted in 1707. The mountain is a symbol of Japan and is often depicted in art and literature. It is a popular destination for hikers and tourists"
+        />
+        <Entry
+        img = {{src: "src/assets/me2.jpg", alt: "Mount Fuji"}}
+        title="Mount Fuji"
+        country="Japan"
+        googleMapsUrl="https://www.google.com/maps/place/Japan"
+        dates="12 Jan, 2021 - 24 Jan, 2021"
+        description="Mount Fuji is Japan's highest peak, standing at 3,776 meters (12,389 feet). It is an active stratovolcano that last erupted in 1707. The mountain is a symbol of Japan and is often depicted in art and literature. It is a popular destination for hikers and tourists"
+        />
+        <Entry
+        img = {{src: "src/assets/me.jpg", alt: "Mount Fuji"}}
+        title="Mount Fuji"
+        country="Japan"
+        googleMapsUrl="https://www.google.com/maps/place/Japan"
+        dates="12 Jan, 2021 - 24 Jan, 2021"
+        description="Mount Fuji is Japan's highest peak, standing at 3,776 meters (12,389 feet). It is an active stratovolcano that last erupted in 1707. The mountain is a symbol of Japan and is often depicted in art and literature. It is a popular destination for hikers and tourists"
+        />
+
+      </main>
+       */
+/* <main>{persons}</main> */
+
+// <div className="contact">
+//   <Contact
+//    img="src/assets/me.jpg"
+//    name="Rene Youssef"
+//    phone="+961 89 897 974"
+//    email="reneYoussef10@gmail.com"
+//    />
+//   <Contact
+//   img="src/assets/me.jpg"
+//   name="Elie Youssef"
+//   phone="+961 79 118 999"
+//   email="elie10@gmail.com"
+//   />
+//   <Contact
+//   img="src/assets/me.jpg"
+//   name="Charbel Youssef"
+//   phone="+961 81 324 974"
+//   email="CharbelYoussef10@gmail.com"
+//   />
+//   <Contact
+//   img="src/assets/me.jpg"
+//   name="George Youssef"
+//   phone="+961 71 332 231"
+//   email="GeorgeYoussef10@gmail.com"
+//   />
+// </div>
+
+// <>
+//   <main>
+//     <Joke
+//       setup="I told my wife she was drawing her eyebrows too high—she looked surprised."
+//       punchline="Surprised? More like shocked!"
+//       upVotes={10}
+//       isPun={true}
+//       comment={[
+//         { author: "Rene", text: "I love this joke!" },
+//         { author: "Elie", text: "I love this joke!" },
+//       ]}
+//     />
+//     <Joke
+//       setup="Parallel lines have so much in common—it's a shame they'll never meet."
+//       punchline="It’s a tragic tale of unrequited geometry."
+//       isPun={true}
+//     />
+//     <Joke
+//       setup="I’m on a seafood diet—I see food and I eat it."
+//       punchline="I’m doing it for the 'crab' in me."
+//       isPun={true}
+//     />
+//     <Joke
+//       setup="I used to play piano by ear, but now I use my hands."
+//       punchline="Guess that’s what happens when you go off the scales."
+//       isPun={false}
+//     />
+//     <Joke
+//       setup="I’m reading a book on anti-gravity—it’s impossible to put down."
+//       punchline="It’s a real uplifting experience!"
+//       isPun={true}
+//     />
+//     <Joke punchline="It’s a real uplifting experience!" isPun={false} />
+//   </main>
+// </>
--- src/Components/Entry.jsx
+++ src/Components/Entry.jsx
@@ -1,23 +1,21 @@
-export default function Entry() {
+export default function Entry(props) {
   return (
 
     <article class="journal-entry">
     <div class="main-img-container">
-      <img class="main-img" src="src/assets/me.jpg" alt="Mount Fuji" />
+      <img class="main-img" src= {props.img.src} alt={props.img.alt}/>
     </div>
 
     <div class="info-container">
       <div class="location-row">
         <img src="src/assets/location.png" class="entry-location" />
-        <span class="country">JAPAN</span>
-        <a href="https://www.google.com/maps/place/Japan" class="entry-map">View on Google Maps</a>
+        <span class="country">{props.country}</span>
+        <a href={props.googleMapsUrl} class="entry-map">View on Google Maps</a>
       </div>
 
-      <h2 class="entry-title">Mount Fuji</h2>
-      <p class="entry-date">12 Jan, 2021 - 24 Jan, 2021</p>
-      <p class="entry-text">
-        Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet).Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
-      </p>
+      <h2 class="entry-title">{props.title}</h2>
+      <p class="entry-date">{props.dates}</p>
+      <p class="entry-text">{props.description}</p>
     </div>
   </article>
         
