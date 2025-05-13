b2f3a85990068ddb30babb04d0f1280ca17732f4
Rene A Youssef <Reneabdo10@gmail.com>
2025-05-13 11:49:31 +0300

    conditinal rendering in joke section using React setState adding hide functionality

--- src/Components/joke.jsx
+++ src/Components/joke.jsx
@@ -13,6 +13,8 @@ export default function Joke(props) {
   we show them if one is not available  we only 
 show the available ones */}
 
+
+      {/* //{props.setup && <p>setup: {props.setup}</p>} */}
       {props.setup ? <p>setup: {props.setup}</p>: null}
       {isShown ? <p>punchline: {props.punchline}</p> : null}
 
